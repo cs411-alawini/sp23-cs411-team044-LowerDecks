27f3149c9af13751aca4816377f8a3f1786606aa
chenghan <johnjiang0409@gmail.com>
2023-04-26 22:25:12 -0500

    transaction queries update

--- Code/Inserting dummy entries.sql
+++ Code/Inserting dummy entries.sql
@@ -7,6 +7,7 @@ SELECT * FROM Coordinate WHERE id = 999999;
 SELECT * FROM Coordinate WHERE id IN (SELECT id FROM Locations WHERE unique_system_identifier = 111);
 SELECT * FROM Locations WHERE unique_system_identifier = 111;
 SELECT id FROM Locations WHERE unique_system_identifier = 111;
+SELECT id FROM t1;
 -- SELECT id FROM Locations WHERE unique_system_identifier = 111;
 -- SELECT * FROM Coordinate LIMIT 5;
 
@@ -27,3 +28,10 @@ VALUES (999999, 37.7749, 122.4194, NULL, 'N', 122.4194, 37.7749, NULL, 'W');
 -- Insert data into Locations table
 INSERT INTO Locations (unique_system_identifier, location_number, location_address, location_city, location_county, location_state, id)
 VALUES (111, 1, '123 Main St', 'Anytown', 'Any County', 'CA', 999999);
+
+
+
+INSERT INTO License (unique_system_identifier, name, email, street_address, city, state)
+VALUES (222, 'John Doe 2', 'johndoe@example.com', '123 Main St', 'Anytown', 'CA');
+INSERT INTO Locations (unique_system_identifier, location_number, location_address, location_city, location_county, location_state, id)
+VALUES (222, 1, '321 Main St', 'Anytown', 'Any County', 'CA', 999999);
--- Code/Transaction.sql
+++ Code/Transaction.sql
@@ -1,11 +1,23 @@
 -- DROP TRIGGER DeletePathsAndLocations;
 SELECT id FROM Locations WHERE unique_system_identifier = 111;
-
+DROP TABLE t1;
+DROP TABLE t2;
 
 DELIMITER //
 
 START TRANSACTION;
 SET @USI = 111;
+DROP TABLE IF EXISTS t1, t2, t3;
+CREATE TABLE t1 (
+	SELECT DISTINCT id FROM Locations WHERE unique_system_identifier = @USI);
+CREATE TABLE t2 (
+	SELECT DISTINCT id FROM Locations loc
+  WHERE id IN ( SELECT id FROM t1 ) AND loc.unique_system_identifier != @USI);
+CREATE TABLE t3 (
+	SELECT id FROM t1
+    WHERE id NOT IN	(SELECT id FROM t2)  );
+-- DELETE FROM t1 WHERE id IN ( SELECT id FROM t2 ) AND id <> -100;
+
 DROP TRIGGER IF EXISTS DeletePathsAndLocations;
 CREATE TRIGGER DeletePathsAndLocations
 BEFORE DELETE
@@ -15,18 +27,17 @@ BEGIN
   DECLARE done BOOLEAN DEFAULT FALSE;
   DECLARE curr_id INT;
   DECLARE ids CURSOR FOR
-    SELECT id FROM Locations WHERE unique_system_identifier = @USI;
+    SELECT id FROM t3;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
+  
   DELETE FROM Path WHERE unique_system_identifier = @USI;
-
   DELETE FROM Locations WHERE unique_system_identifier = @USI;
 
   OPEN ids;
 
   REPEAT
-    FETCH NEXT FROM ids INTO curr_id;
     DELETE FROM Coordinate WHERE id = curr_id;
+    FETCH NEXT FROM ids INTO curr_id;
   UNTIL done
   END REPEAT;
 
